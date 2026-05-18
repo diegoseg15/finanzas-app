f016b52a7b2dd3d96705fe9d1c1f3f2d0c71a0d1
diegoseg15 <diegofersv@gmail.com>
2026-05-18 10:51:08 -0500

    feat: add transfer store with balance updates

---
+++ src/store/useTransferStore.ts
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+
+import {
+    createTransfer,
+    getTotalDebitFromOrigin,
+} from "@/services/transfer.service";
+import { useAccountStore } from "@/store/useAccountStore";
+import { CreateTransferInput, Transfer } from "@/types/finance.types";
+
+type TransferState = {
+  transfers: Transfer[];
+
+  addTransfer: (input: CreateTransferInput) => void;
+  getTransfersByAccountId: (accountId: string) => Transfer[];
+};
+
+export const useTransferStore = create<TransferState>((set, get) => ({
+  transfers: [],
+
+  addTransfer: (input) => {
+    const newTransfer = createTransfer(input);
+
+    set((state) => ({
+      transfers: [newTransfer, ...state.transfers],
+    }));
+
+    if (newTransfer.status !== "confirmed") {
+      return;
+    }
+
+    const accountStore = useAccountStore.getState();
+
+    const originDebit = getTotalDebitFromOrigin(
+      newTransfer.fromAmount,
+      newTransfer.feeAmount,
+      newTransfer.feeCurrency === newTransfer.fromCurrency,
+    );
+
+    accountStore.applyAccountBalanceChange(
+      newTransfer.fromAccountId,
+      newTransfer.fromCurrency,
+      -originDebit,
+    );
+
+    accountStore.applyAccountBalanceChange(
+      newTransfer.toAccountId,
+      newTransfer.toCurrency,
+      newTransfer.toAmount,
+    );
+
+    if (
+      newTransfer.feeAmount > 0 &&
+      newTransfer.feeCurrency !== newTransfer.fromCurrency
+    ) {
+      accountStore.applyAccountBalanceChange(
+        newTransfer.fromAccountId,
+        newTransfer.feeCurrency,
+        -newTransfer.feeAmount,
+      );
+    }
+  },
+
+  getTransfersByAccountId: (accountId) => {
+    return get().transfers.filter(
+      (transfer) =>
+        transfer.fromAccountId === accountId ||
+        transfer.toAccountId === accountId,
+    );
+  },
+}));
