02b53d374b1a5dfe41da9129e30d8b1a95324980
diegoseg15 <diegofersv@gmail.com>
2026-05-18 11:14:58 -0500

    feat: persist transfer store

--- src/store/useTransferStore.ts
+++ src/store/useTransferStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
+import { appStorage } from "@/services/storage/app-storage.service";
 import {
-    createTransfer,
-    getTotalDebitFromOrigin,
+  createTransfer,
+  getTotalDebitFromOrigin,
 } from "@/services/transfer.service";
 import { useAccountStore } from "@/store/useAccountStore";
 import { CreateTransferInput, Transfer } from "@/types/finance.types";
@@ -14,57 +16,68 @@ type TransferState = {
   getTransfersByAccountId: (accountId: string) => Transfer[];
 };
 
-export const useTransferStore = create<TransferState>((set, get) => ({
-  transfers: [],
+export const useTransferStore = create<TransferState>()(
+  persist(
+    (set, get) => ({
+      transfers: [],
 
-  addTransfer: (input) => {
-    const newTransfer = createTransfer(input);
+      addTransfer: (input) => {
+        const newTransfer = createTransfer(input);
 
-    set((state) => ({
-      transfers: [newTransfer, ...state.transfers],
-    }));
+        set((state) => ({
+          transfers: [newTransfer, ...state.transfers],
+        }));
 
-    if (newTransfer.status !== "confirmed") {
-      return;
-    }
+        if (newTransfer.status !== "confirmed") {
+          return;
+        }
 
-    const accountStore = useAccountStore.getState();
+        const accountStore = useAccountStore.getState();
 
-    const originDebit = getTotalDebitFromOrigin(
-      newTransfer.fromAmount,
-      newTransfer.feeAmount,
-      newTransfer.feeCurrency === newTransfer.fromCurrency,
-    );
+        const originDebit = getTotalDebitFromOrigin(
+          newTransfer.fromAmount,
+          newTransfer.feeAmount,
+          newTransfer.feeCurrency === newTransfer.fromCurrency,
+        );
 
-    accountStore.applyAccountBalanceChange(
-      newTransfer.fromAccountId,
-      newTransfer.fromCurrency,
-      -originDebit,
-    );
+        accountStore.applyAccountBalanceChange(
+          newTransfer.fromAccountId,
+          newTransfer.fromCurrency,
+          -originDebit,
+        );
 
-    accountStore.applyAccountBalanceChange(
-      newTransfer.toAccountId,
-      newTransfer.toCurrency,
-      newTransfer.toAmount,
-    );
+        accountStore.applyAccountBalanceChange(
+          newTransfer.toAccountId,
+          newTransfer.toCurrency,
+          newTransfer.toAmount,
+        );
 
-    if (
-      newTransfer.feeAmount > 0 &&
-      newTransfer.feeCurrency !== newTransfer.fromCurrency
-    ) {
-      accountStore.applyAccountBalanceChange(
-        newTransfer.fromAccountId,
-        newTransfer.feeCurrency,
-        -newTransfer.feeAmount,
-      );
-    }
-  },
+        if (
+          newTransfer.feeAmount > 0 &&
+          newTransfer.feeCurrency !== newTransfer.fromCurrency
+        ) {
+          accountStore.applyAccountBalanceChange(
+            newTransfer.fromAccountId,
+            newTransfer.feeCurrency,
+            -newTransfer.feeAmount,
+          );
+        }
+      },
 
-  getTransfersByAccountId: (accountId) => {
-    return get().transfers.filter(
-      (transfer) =>
-        transfer.fromAccountId === accountId ||
-        transfer.toAccountId === accountId,
-    );
-  },
-}));
+      getTransfersByAccountId: (accountId) => {
+        return get().transfers.filter(
+          (transfer) =>
+            transfer.fromAccountId === accountId ||
+            transfer.toAccountId === accountId,
+        );
+      },
+    }),
+    {
+      name: "finance-app-transfers",
+      storage: createJSONStorage(() => appStorage),
+      partialize: (state) => ({
+        transfers: state.transfers,
+      }),
+    },
+  ),
+);
